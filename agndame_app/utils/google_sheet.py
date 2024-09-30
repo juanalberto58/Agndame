765e95a68ee393bc1426665c70df16348ab683af
juanalberto58 <juanalberto580@gmail.com>
2024-09-30 20:05:05 +0200

    Plantanado clase google sheet

---
+++ agndame_app/utils/google_sheet.py
@@ -0,0 +1,70 @@
+import os.path
+import gspread
+import pandas as pd
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+SAMPLE_RANGE_NAME = "Class Data!A2:E"
+
+class GoogleSheetManager:
+    def __init__(self):
+        self.service = self._autheticate
+
+    # Metodo para autenticarse
+    def _autheticate(self):
+        """Shows basic usage of the Google Calendar API.
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                CREDENTIALS_PATH = os.path.join(BASE_DIR, '', 'utils', 'credentials.json')
+
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    CREDENTIALS_PATH, SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        return build("calendar", "v3", credentials=creds)
+        
+
+    def list_sheets(self):
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+
+        if not values:
+            print("No data found.")
+            return
+
+        print("Name, Major:")
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print(f"{row[0]}, {row[4]}")
+
+
